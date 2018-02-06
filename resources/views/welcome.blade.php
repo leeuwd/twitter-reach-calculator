e92ce9cbd816612ff5513ee840e98d26afc44eee
Dick de Leeuw <dick@leeuwmedia.nl>
2018-02-06 21:45:16 +0100

    Code style change.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <title>{{ config('app.name', 'Twitter reach calculator') }}</title>
-        @include('components.meta.head')
-    </head>
-    <body>
-       <article class="wizard">
-           hello world
-       </article>
-    </body>
+<head>
+    <meta charset="utf-8"/>
+    <title>{{ config('app.name', 'Twitter reach calculator') }}</title>
+    @include('components.meta.head')
+</head>
+<body>
+<article class="wizard">
+    hello world
+</article>
+</body>
 </html>
