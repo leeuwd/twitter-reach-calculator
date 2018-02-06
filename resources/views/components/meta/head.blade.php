fb00539d8458fb306f5fc833d2b00e4fbff03566
Dick de Leeuw <dick@leeuwmedia.nl>
2018-02-06 21:28:42 +0100

    Basic theme scaffolding.

---
+++ resources/views/components/meta/head.blade.php
@@ -0,0 +1,12 @@
+<meta name="author" content="Dick de Leeuw"/>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+<meta name="mobile-web-app-capable" content="yes"/>
+<meta name="apple-mobile-web-app-capable" content="yes"/>
+<meta name="application-name" content="{{ config('app.name', 'Twitter reach calculator') }}"/>
+<meta name="apple-mobile-web-app-title" content="{{ config('app.name', 'Twitter reach calculator') }}"/>
+<meta name="theme-color" content="#252830"/>
+<meta name="msapplication-navbutton-color" content="#ffffff"/>
+<meta name="msapplication-starturl" content="/"/>
+<meta name="csrf-token" content="{{ csrf_token() }}"/>
+
+<link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
