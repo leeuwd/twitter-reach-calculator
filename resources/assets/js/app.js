8d0b1ecd86a7719e0191f9289c90dbe37f54ce47
Dick de Leeuw <dick@leeuwmedia.nl>
2018-02-10 16:44:23 +0100

    Javascript scaffolding.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,8 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
+// Bootstrap
 require('./bootstrap');
 
-window.Vue = require('vue');
+// Components
+require('./components/_app');
+require('./components/_log');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+window.twitter.App.init();
