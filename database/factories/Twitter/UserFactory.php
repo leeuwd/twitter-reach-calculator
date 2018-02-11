0ecb93c38353f0b200e1d46a60a1e18ce168a500
Dick de Leeuw <dick@leeuwmedia.nl>
2018-02-11 18:36:59 +0100

    Model factory.

---
+++ database/factories/Twitter/UserFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+/* @var $factory Illuminate\Database\Eloquent\Factory */
+
+use App\Twitter\Models\User;
+use Carbon\Carbon;
+use Faker\Generator as Faker;
+
+// Default User
+$factory->define(User::class, function (Faker $faker) {
+    $randomDate = $faker->dateTimeBetween('-30 days', '+30 days');
+
+    return [
+        'id'                      => $faker->numberBetween(10000000000000000, 900000000000000000),
+        'name'                    => $faker->name,
+        'screen_name'             => $faker->firstName,
+        'location'                => $faker->city . ' ' . $faker->country,
+        'description'             => $faker->sentence,
+        'url'                     => $faker->url,
+        'lang'                    => $faker->countryCode,
+        'profile_image_url_https' => $faker->url,
+        'followers_count'         => $faker->numberBetween(0, 1000000),
+        'friends_count'           => $faker->numberBetween(0, 1000000),
+        'listed_count'            => $faker->numberBetween(0, 1000000),
+        'favourites_count'        => $faker->numberBetween(0, 1000000),
+        'statuses_count'          => $faker->numberBetween(0, 1000000),
+        'time_zone'               => $faker->timezone,
+        'geo_enabled'             => $faker->boolean,
+        'verified'                => $faker->boolean,
+        'created_at'              => Carbon::createFromTimestamp($randomDate->getTimestamp()),
+    ];
+});
+
+// Leecher
+$factory->state(User::class, 'leecher', function () {
+    return [
+        'statuses_count' => 0,
+    ];
+});
