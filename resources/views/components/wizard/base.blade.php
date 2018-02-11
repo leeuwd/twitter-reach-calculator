9a3b05aa46bd14af37fd11f5bfcef8a69f8f3f79
Dick de Leeuw <dick@leeuwmedia.nl>
2018-02-11 11:34:41 +0100

    UI/UX improvements.

--- resources/views/components/wizard/base.blade.php
+++ resources/views/components/wizard/base.blade.php
@@ -1,4 +1,4 @@
-<article class="wizard wizard--green">
+<article class="wizard wizard--purple">
     {!! Form::open(['url' => route('getReach'), 'class' => 'wizard__form']) !!}
     <h1 class="wizard__title">@lang('wizard.title')</h1>
     <p class="wizard__description">@lang('wizard.description')</p>
